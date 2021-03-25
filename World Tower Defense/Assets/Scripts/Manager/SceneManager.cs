a5b2f3485cc8cdcac44f1dc58271713b29a2bf69
syk_09@naver.com <syk_09@naver.com>
2021-03-25 21:53:24 +0900

    feat : Add SceneManager for Loading Scene

--- World Tower Defense/Assets/Scripts/Manager/SceneManager.cs
+++ World Tower Defense/Assets/Scripts/Manager/SceneManager.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManager : UnitySingleton<SceneManager>
 {
-    private void Awake()
+    public override void OnCreated()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    public override void OnInitiate()
+    {
+        
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 }
