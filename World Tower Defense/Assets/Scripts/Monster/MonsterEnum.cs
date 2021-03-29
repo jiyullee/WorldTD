095d4d04e7e9f4c8fa8a8fb40e1f2aaa04499e87
jsuk10 <jsuk10@naver.com>
2021-03-30 01:31:07 +0900

    feat : Add monsterEnum

---
+++ World Tower Defense/Assets/Scripts/Monster/MonsterEnum.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//몬스터 관련 enum 모음 스크립트
+
+
+[System.Serializable]
+public enum Difficulty
+{
+    easy,
+    nomal,
+    hard
+}
