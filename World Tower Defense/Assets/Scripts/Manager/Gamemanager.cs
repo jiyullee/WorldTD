0cab0b58b4cf3d511d0a4fb129f0c8c2a1712728
jsuk10 <jsuk10@naver.com>
2021-03-31 01:36:17 +0900

    feat : Add GameManager (frame)
    게임메니저 틀만 생성

---
+++ World Tower Defense/Assets/Scripts/Manager/Gamemanager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class Gamemanager : UnitySingleton<Gamemanager>
+{
+    [SerializeField]
+    private const int MaxHp = 30;
+    private int hp;
+    public override void OnCreated()
+    {
+    }
+
+
+    public override void OnInitiate()
+    {
+        hp = MaxHp;
+    }
+
+
+    //일시정지시 0, 배율시 맞는 배율 삽입
+    public void TimeSetting(float Magnification)
+    {
+        // TimeManager.TimeAcceleration(Magnification);
+    }
+    //게임 클리어
+    public void GameClear()
+    {
+    }
+    //게임에서 짐.
+    public void GameOver()
+    {
+        Debug.Log("GameOver");
+        //게임 오버 UI표기.
+    }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit() // 어플리케이션 종료
+#endif
+    }
+}
