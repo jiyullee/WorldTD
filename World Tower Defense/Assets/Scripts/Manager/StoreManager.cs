dd50ed7db886ca29a69ed83e92e62b3b82ccc7c1
jiyullee <syk_09@naver.com>
2021-04-06 00:28:10 +0900

    feat : Add StoreManager for Managing store

--- World Tower Defense/Assets/Scripts/Manager/StoreManager.cs
+++ World Tower Defense/Assets/Scripts/Manager/StoreManager.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoreManager : MonoBehaviour
+public class StoreManager : UnitySingleton<StoreManager>
 {
     Dictionary<int, Tower> all_towers_dic = new Dictionary<int, Tower>();
     Dictionary<int, Tower> remain_towers_dic = new Dictionary<int, Tower>();
-    
-    void Awake()
+
+    public override void OnCreated()
     {
         
     }
+
+    public override void OnInitiate()
+    {
+        
+    }
+
+
 }
