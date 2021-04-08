54ae2375db17a8daea792b5a4f47f9407aac5cc0
jsuk10 <jsuk10@naver.com>
2021-04-09 01:00:34 +0900

    feat : Add if Attack Calling GetDamage()

--- World Tower Defense/Assets/Scripts/Game/Bullet.cs
+++ World Tower Defense/Assets/Scripts/Game/Bullet.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Bullet : PollingObject
 {
     private float speed;
+    private float damage;
     private PollingObject target;
     public override void OnCreated()
     {
@@ -24,10 +25,11 @@ public class Bullet : PollingObject
         Polling2.ReturnObject(this);
     }
 
-    public void SpawnTo(PollingObject p_target, float p_speed)
+    public void SpawnTo(PollingObject p_target, float p_speed, float p_damage)
     {
         target = p_target;
         speed = p_speed;
+        damage = p_damage;
         //Invoke("DestroySelf", 1 / speed);
     }
 
@@ -35,7 +37,8 @@ public class Bullet : PollingObject
     {
         if (target != null)
         {
-            transform.position = Vector3.Lerp(transform.position, target.transform.position, speed * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, target.transform.position, 5 * Time.deltaTime);
+            // transform.position = Vector3.Lerp(transform.position, target.transform.position, speed * Time.deltaTime);
         }
         if (target.gameObject.activeSelf == false)
             DestroySelf();
@@ -43,7 +46,10 @@ public class Bullet : PollingObject
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<PollingObject>() == target)
-            DestroySelf();
+        Monster monster = other.gameObject.GetComponent<Monster>();
+        if (monster == target)
+        {
+            monster.GetDamage((int)damage);
+        }
     }
 }
