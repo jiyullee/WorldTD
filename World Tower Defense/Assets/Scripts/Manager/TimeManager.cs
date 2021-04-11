3af819c54f60d722a08f830a58ca92bc163830cc
jsuk10 <jsuk10@naver.com>
2021-04-11 19:47:03 +0900

    feat : Add TimeManager

---
+++ World Tower Defense/Assets/Scripts/Manager/TimeManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : UnitySingleton<TimeManager>
+{
+    private float tempTime = 1.0f;
+
+    public override void OnCreated()
+    {
+    }
+
+    public override void OnInitiate()
+    {
+        InitTime();
+    }
+    public void InitTime()
+    {
+        ChangeSpeed(1.0f);
+    }
+    public void ChangeSpeed(float time)
+    {
+        Time.timeScale = time;
+        Time.fixedDeltaTime = 0.02F * Time.timeScale;
+    }
+
+    public void Pause()
+    {
+        tempTime = Time.timeScale;
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0.02F * Time.timeScale;
+    }
+
+    public void startTime()
+    {
+        Time.timeScale = tempTime;
+        Time.fixedDeltaTime = 0.02F * Time.timeScale;
+    }
+}
